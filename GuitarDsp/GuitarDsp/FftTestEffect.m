d2140e4bc2bb33e2c88f3f65e57f9ae14ec83f92
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2017-03-12 14:12:53 +0100

    single segment filtering

--- GuitarDsp/GuitarDsp/FftTestEffect.m
+++ GuitarDsp/GuitarDsp/FftTestEffect.m
@@ -20,6 +20,12 @@
 
 @property (nonatomic, assign) int xD;
 
+@property (nonatomic, assign) int k;
+
+
+@property (nonatomic, assign) float *circularBuffer;
+//@property (nonatomic, assign) int circularBufferSize;
+
 @end
 
 @implementation FftTestEffect
@@ -43,6 +49,7 @@
     int n = 1 << log2n;
     assert(n == maxFrames);
     float nOver2 = maxFrames/2;
+    self.k = 0;
     
 //    DSPSplitComplex A;
     A.realp = (float*)malloc(nOver2 * sizeof(float));
@@ -54,39 +61,75 @@
 }
 
 - (void)processSample:(struct Sample)inputSample intoBuffer:(float *)outputBuffer {
+    float *input = malloc(sizeof(float) * 10);
+    for (int i = 0; i < 10; i ++) {
+        input[i] = i + 1;
+    }
     
     
-//    if (_xD++ % 100 < 50) {
-//        memcpy(outputBuffer, inputSample.amp, self.samplingSettings.packetByteSize);
-//        return;
-//    }
     
-    float *real = inputSample.amp;
-    float *imaginary = malloc(self.samplingSettings.packetByteSize);
-    DSPSplitComplex splitComplex;
-    splitComplex.realp = real;
-    splitComplex.imagp = imaginary;
+    const vDSP_Stride
+    SignalStride = 1,
+    FilterStride = 1,
+    ResultStride = 1;
     
-    vDSP_Length length = (long)floor(log2(self.samplingSettings.framesPerPacket));
+    vDSP_Length
+    FilterLength = 3,
+    SignalLength = 5,
+    ResultLength = SignalLength + FilterLength - 1;
     
-    vDSP_fft_zip(self.fftSetup, &splitComplex, 1, length, FFT_FORWARD);
     
-    for (int i = 0; i < self.samplingSettings.framesPerPacket; i++) {
-        if (i < 50) {
-            continue;
+    float *Signal, *Filter, *Result;
+    
+    Signal = malloc(ResultLength * SignalStride * sizeof *Signal);
+    Filter = malloc(FilterLength * FilterStride * sizeof *Filter);
+    Result = malloc(ResultLength * ResultStride * sizeof *Result);
+    
+    bzero(Signal, SignalStride * sizeof *Signal);
+    memcpy(&Signal[FilterLength - 1], input, sizeof(float) * SignalLength);
+    
+    for (int i = 0; i < FilterLength; i++) {
+        Filter[i*FilterStride] = 0.0;
+    }
+    Filter[0*FilterStride] = 1.0;
+    Filter[1*FilterStride] = 1.0;
+    
+    
+//    [self convSignal:Signal signalLength:SignalLength filter:Filter filterLength:FilterLength output:Result];
+    vDSP_conv(Signal, SignalStride, Filter + FilterLength - 1, -1,
+              Result, ResultStride, ResultLength, FilterLength);
+    
+    
+    NSMutableString *signalString = [@"S: " mutableCopy];
+    for (int i = 0; i < ResultLength; i++) {
+        [signalString appendString:[NSString stringWithFormat:@"%.1f ", Signal[i]]];
+    }
+    NSMutableString *filterString = [@"F: " mutableCopy];
+    for (int i = 0; i < FilterLength; i++) {
+        [filterString appendString:[NSString stringWithFormat:@"%.1f ", Filter[i]]];
+    }
+    NSMutableString *resultString = [@"R: " mutableCopy];
+    for (int i = 0; i < ResultLength; i++) {
+        [resultString appendString:[NSString stringWithFormat:@"%.1f ", Result[i]]];
+    }
+    
+    NSLog(@"\n%@\n%@\n%@\n", signalString, filterString, resultString);
+}
+
+- (void)convSignal:(float *)signal signalLength:(int)signalLength
+            filter:(float *)filter filterLength:(int)filterLength
+            output:(float *)output {
+    int resultLength = signalLength + filterLength - 1;
+    
+    bzero(output, sizeof(float) * resultLength);
+    
+    for (int i = 0; i < signalLength; i++) {
+        for (int j = 0; j < filterLength; j++) {
+            output[i + j] += signal[i] * filter[j];
         }
-        splitComplex.realp[i] = 0;
-        splitComplex.imagp[i] = 0;
     }
-    
-    vDSP_fft_zip(self.fftSetup, &splitComplex, 1, length, FFT_INVERSE);
-    
-    for (int i = 0; i < self.samplingSettings.framesPerPacket; i++) {
-        splitComplex.realp[i] /= (float)self.samplingSettings.framesPerPacket;
-    }
-    
-    memcpy(outputBuffer, splitComplex.realp, self.samplingSettings.packetByteSize);
     
 }
 
 @end
+

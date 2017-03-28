a6bcd1cb14a77353b993c5d3b786840f31d4b5d6
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2017-03-29 01:06:25 +0200

    guitar dsp module map

--- Bow/Bow/AppDelegate.swift
+++ Bow/Bow/AppDelegate.swift
@@ -7,20 +7,17 @@
 //
 
 import Cocoa
+import GuitarDsp
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
 

05d6ba1172bd0b5c243c4272be8946b289b4c321
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2017-04-09 14:01:37 +0200

    effect cretor

---
+++ EffectCreator/EffectCreator/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  EffectCreator
+//
+//  Created by Maciej Chmielewski on 09.04.2017.
+//  Copyright © 2017 Maciej Chmielewski. All rights reserved.
+//
+
+import Cocoa
+import GuitarDsp
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let processor = Processor(samplingSettings: AudioInterface.shared().samplingSettings, tempo: 120)
+        let board = Board()
+        board.effects = [makeDevelopmentEffect()]
+        processor.activeBoard = board
+        AudioInterface.shared().use(processor)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+    func makeDevelopmentEffect() -> Effect {
+        return AmpEffect(samplingSettings: AudioInterface.shared().samplingSettings)
+    }
+
+}
+
---
+++ EffectCreator/EffectCreator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EffectCreator
+//
+//  Created by Maciej Chmielewski on 09.04.2017.
+//  Copyright © 2017 Maciej Chmielewski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

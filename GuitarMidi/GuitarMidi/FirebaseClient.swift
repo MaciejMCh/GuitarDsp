cce5f066f53d268c02b8e49be6caa89343678aea
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2017-11-16 18:53:12 +0100

    samples uploading logic

--- GuitarMidi/GuitarMidi/FirebaseClient.swift
+++ GuitarMidi/GuitarMidi/FirebaseClient.swift
@@ -19,15 +19,26 @@ class FirebaseClient {
     }
     
     private func syncSamples() {
-        for samplePath in indexSamples() {
-            let sampleName = samplePath.components(separatedBy: "samples/").last!
-            Storage.storage().reference(withPath: sampleName).getMetadata { (metadata, error) in
-                if metadata == nil {
-                    let sampleId = UUID().uuidString
-                    Storage.storage().reference(withPath: "samples/\(sampleId)").putFile(from: URL(fileURLWithPath: samplePath))
-                    Database.database().reference(withPath: "samples").updateChildValues([sampleId: sampleName])
-                }
+        Database.database().reference(withPath: "samples").observeSingleEvent(of: .value) { (dataSnapshot: DataSnapshot) in
+            let samplesRecordsJson = dataSnapshot.value as? [String: String] ?? [:]
+            let serverSamplesRecords: [(id: String, path: String)] = samplesRecordsJson.map{(id: $0.key as! String, path: $0.value as! String)}
+            let serverSamplesPaths = serverSamplesRecords.map{$0.path}
+            var newServerSamplesRecords = serverSamplesRecords
+            
+            let localSamplesFilesPaths = self.indexSamples()
+            for localSampleFilePath in localSamplesFilesPaths {
+                let localSamplePath = localSampleFilePath.components(separatedBy: "samples/").last!
+                if serverSamplesPaths.contains(localSamplePath) {continue}
+                let newSampleRecord = (id: NSUUID().uuidString, path: localSamplePath)
+                newServerSamplesRecords.append(newSampleRecord)
+                Storage.storage().reference(withPath: "samples/\(newSampleRecord.id)").putFile(from: URL(fileURLWithPath: localSampleFilePath))
             }
+            
+            var newSamplesRecordsJson: [String: String] = [:]
+            for newServerSamplesRecord in newServerSamplesRecords {
+                newSamplesRecordsJson[newServerSamplesRecord.id] = newServerSamplesRecord.path
+            }
+            Database.database().reference(withPath: "samples").updateChildValues(newSamplesRecordsJson)
         }
     }
     
